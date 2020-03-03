ccf9a8ceabb7d4608ef1254b203f61572b28f6b2
Frode Hansen <frode.hansen@bekk.no>
2020-03-03 20:21:49 +0100

    Add support for inputMode on FormikInput

--- src/typed-formik-form/components/formik-input/FormikInput.tsx
+++ src/typed-formik-form/components/formik-input/FormikInput.tsx
@@ -6,14 +6,35 @@ import { getFeilPropForFormikInput } from '../../utils/typedFormErrorUtils';
 import LabelWithInfo from '../helpers/label-with-info/LabelWithInfo';
 import { TypedFormikFormContext } from '../typed-formik-form/TypedFormikForm';
 
+type InputMode = 'none' | 'text' | 'tel' | 'url' | 'email' | 'numeric' | 'decimal' | 'search';
+
 interface OwnProps<FieldName> extends Omit<InputProps, 'name'> {
     name: FieldName;
+    inputmode?: InputMode;
 }
+
+const getPatternForInputMode = (inputMode?: InputMode): string | undefined => {
+    switch (inputMode) {
+        case 'numeric':
+            return '[0-9]*';
+        default:
+            return undefined;
+    }
+};
 
 export type FormikInputProps<FieldName> = OwnProps<FieldName> & TypedFormInputCommonProps;
 
-function FormikInput<FieldName>({ label, name, info, feil, validate, ...restProps }: FormikInputProps<FieldName>) {
+function FormikInput<FieldName>({
+    label,
+    name,
+    info,
+    feil,
+    validate,
+    inputMode,
+    ...restProps
+}: FormikInputProps<FieldName>) {
     const context = React.useContext(TypedFormikFormContext);
+
     return (
         <Field validate={validate} name={name}>
             {({ field, form }: FieldProps) => {
@@ -21,6 +42,8 @@ function FormikInput<FieldName>({ label, name, info, feil, validate, ...restProp
                     <Input
                         {...restProps}
                         {...field}
+                        inputMode={inputMode}
+                        pattern={getPatternForInputMode(inputMode)}
                         label={<LabelWithInfo info={info}>{label}</LabelWithInfo>}
                         feil={getFeilPropForFormikInput({ field, form, context, feil })}
                         value={field.value === undefined ? '' : field.value}
