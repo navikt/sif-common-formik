7ef223013b5726959b7b08eff80f235a81c02ff9
Frode Hansen <frode.hansen@bekk.no>
2020-03-24 21:17:06 +0100

    Fix width issue on labelWithInfo icon

---
+++ src/typed-formik-form/components/formik-fnr-input/FormikFnrInput.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { Field, FieldProps } from 'formik';
+import { FnrInput, FnrInputProps } from 'nav-frontend-skjema';
+import { TypedFormInputCommonProps } from '../../types';
+import { getFeilPropForFormikInput } from '../../utils/typedFormErrorUtils';
+import LabelWithInfo from '../helpers/label-with-info/LabelWithInfo';
+import { TypedFormikFormContext } from '../typed-formik-form/TypedFormikForm';
+
+type InputMode = 'none' | 'text' | 'tel' | 'url' | 'email' | 'numeric' | 'decimal' | 'search';
+
+interface OwnProps<FieldName> extends Omit<FnrInputProps, 'name' | 'onValidate'> {
+    name: FieldName;
+    inputmode?: InputMode;
+}
+
+const getPatternForInputMode = (inputMode?: InputMode): string | undefined => {
+    switch (inputMode) {
+        case 'numeric':
+            return '[0-9]*';
+        default:
+            return undefined;
+    }
+};
+
+export type FormikInputProps<FieldName> = OwnProps<FieldName> & TypedFormInputCommonProps;
+
+function FormikFnrInput<FieldName>({
+    label,
+    name,
+    info,
+    feil,
+    validate,
+    inputMode = 'numeric',
+    ...restProps
+}: FormikInputProps<FieldName>) {
+    const context = React.useContext(TypedFormikFormContext);
+
+    return (
+        <Field validate={validate} name={name}>
+            {({ field, form }: FieldProps) => {
+                return (
+                    <FnrInput
+                        {...restProps}
+                        {...field}
+                        onValidate={(valid) => console.log(valid)}
+                        inputMode={inputMode}
+                        pattern={getPatternForInputMode(inputMode)}
+                        label={<LabelWithInfo info={info}>{label}</LabelWithInfo>}
+                        feil={getFeilPropForFormikInput({ field, form, context, feil })}
+                        value={field.value === undefined ? '' : field.value}
+                    />
+                );
+            }}
+        </Field>
+    );
+}
+
+export default FormikFnrInput;
