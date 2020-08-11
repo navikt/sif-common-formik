24a753a5788cfd3bb2f09a1bb5445c7cbca9c6a7
Frode Hansen <frode.hansen@bekk.no>
2020-08-11 10:59:58 +0200

    Extend dialog example

--- src/dev/examples/formik-example/ferieuttak-example/types.ts
+++ src/dev/examples/formik-example/ferieuttak-example/types.ts
@@ -2,8 +2,15 @@ export interface Ferieuttak {
     id?: string;
     fom: Date;
     tom: Date;
+    land: Ferieland[];
 }
 
 export const isFerieuttak = (ferieuttak: Partial<Ferieuttak>): ferieuttak is Ferieuttak => {
-    return ferieuttak.fom !== undefined && ferieuttak.tom !== undefined;
+    return ferieuttak.fom !== undefined && ferieuttak.tom !== undefined && ferieuttak.land !== undefined;
 };
+
+export enum Ferieland {
+    'Norge' = 'norge',
+    'Sverige' = 'sverige',
+    'Danmark' = 'danmark',
+}
