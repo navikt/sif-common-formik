5f01d9bbc21ecf10a6e7ded4cafc678c76fa91c7
Frode Hansen <frode.hansen@bekk.no>
2021-04-19 15:20:24 +0200

    Legge ResultType til validateAll

--- src/typed-formik-form/validation/validationUtils.ts
+++ src/typed-formik-form/validation/validationUtils.ts
@@ -2,7 +2,7 @@ import { ValidationResult } from './types';
 
 export const hasValue = (value: any): boolean => value !== '' && value !== undefined && value !== null;
 
-export const validateAll = (validations: Array<() => ValidationResult<any>>) => {
+export const validateAll = <ResultType = string>(validations: Array<() => ValidationResult<any>>): ResultType => {
     let result: ValidationResult<any>;
     validations.some((validateFunc) => {
         const validationResult = validateFunc();
