03185e3dff3e5f74cf1673e023fab1689de8609f
Frode Hansen <frode.hansen@bekk.no>
2021-04-19 11:11:10 +0200

    Legge til export

--- src/typed-formik-form/validation/index.ts
+++ src/typed-formik-form/validation/index.ts
@@ -1,5 +1,6 @@
 export { default as validateChecked } from './validateChecked';
 export { default as validateDate } from './validateDate';
+export { default as dateRangeValidation } from './dateRangeValidation';
 export { default as validateFødselsnummer } from './validateFødselsnummer';
 export { default as validateList } from './validateList';
 export { default as validateNumber } from './validateNumber';
@@ -8,6 +9,7 @@ export { default as validateString } from './validateString';
 export { default as validateYesOrNoIsAnswered } from './validateYesOrNoIsAnswered';
 export { default as validateRequiredValue } from './validateRequiredValue';
 
+export * from './dateRangeValidation';
 export * from './validateChecked';
 export * from './validateDate';
 export * from './validateFødselsnummer';
