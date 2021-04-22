8e578dc6e8caacd91e431650d9891ab5e00b5ad7
Frode Hansen <frode.hansen@bekk.no>
2021-04-22 14:25:49 +0200

    Endre type på ValidationResult - validateAll

--- src/typed-formik-form/validation/validationUtils.ts
+++ src/typed-formik-form/validation/validationUtils.ts
@@ -1,8 +1,10 @@
-import { ValidationResult } from './types';
+import { ValidationError, ValidationResult } from './types';
 
 export const hasValue = (value: any): boolean => value !== '' && value !== undefined && value !== null;
 
-export const validateAll = <ResultType = string>(validations: Array<() => ValidationResult<any>>): ResultType => {
+export const validateAll = <ResultType = string>(
+    validations: Array<() => ValidationResult<ValidationError>>
+): ResultType => {
     let result: ValidationResult<any>;
     validations.some((validateFunc) => {
         const validationResult = validateFunc();
