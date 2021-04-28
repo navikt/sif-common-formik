02912e3770c1693bfa94a152ab3c13204da82dc5
Frode Hansen <frode.hansen@bekk.no>
2021-04-28 13:24:02 +0200

    onlyWeekdays test på dateValidator

--- src/typed-formik-form/validation/__tests__/validateDate.test.ts
+++ src/typed-formik-form/validation/__tests__/validateDate.test.ts
@@ -19,6 +19,36 @@ describe(`validateDate`, () => {
         expect(getDateValidator()(`asd`)).toBe(ValidateDateError.invalidDateFormat);
         expect(getDateValidator({ required: true })(`asd`)).toBe(ValidateDateError.invalidDateFormat);
     });
+    describe('only weekdays', () => {
+        const monday = '2020-02-17';
+        const tuesday = '2020-02-18';
+        const wednesday = '2020-02-19';
+        const thursday = '2020-02-20';
+        const friday = '2020-02-21';
+        const saturday = '2020-02-22';
+        const sunday = '2020-02-23';
+        it(`returns ${undefined} when date is a monday`, () => {
+            expect(getDateValidator({ onlyWeekdays: true })(monday)).toBe(undefined);
+        });
+        it(`returns ${undefined} when date is a tuesday`, () => {
+            expect(getDateValidator({ onlyWeekdays: true })(tuesday)).toBe(undefined);
+        });
+        it(`returns ${undefined} when date is a wednesday`, () => {
+            expect(getDateValidator({ onlyWeekdays: true })(wednesday)).toBe(undefined);
+        });
+        it(`returns ${undefined} when date is a thursday`, () => {
+            expect(getDateValidator({ onlyWeekdays: true })(thursday)).toBe(undefined);
+        });
+        it(`returns ${undefined} when date is a friday`, () => {
+            expect(getDateValidator({ onlyWeekdays: true })(friday)).toBe(undefined);
+        });
+        it(`returns ${ValidateDateError.dateNotWeekday} when date is a saturday`, () => {
+            expect(getDateValidator({ onlyWeekdays: true })(saturday)).toBe(ValidateDateError.dateNotWeekday);
+        });
+        it(`returns ${ValidateDateError.dateNotWeekday} when date is a sunday`, () => {
+            expect(getDateValidator({ onlyWeekdays: true })(sunday)).toBe(ValidateDateError.dateNotWeekday);
+        });
+    });
     describe('date ranges', () => {
         const min = ISOStringToDate('2020-10-10');
         const max = ISOStringToDate('2020-10-20');
--- src/typed-formik-form/validation/getDateValidator.ts
+++ src/typed-formik-form/validation/getDateValidator.ts
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs';
 import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
 import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
+import isoWeek from 'dayjs/plugin/isoWeek';
 import datepickerUtils from '../components/formik-datepicker/datepickerUtils';
 import { getRequiredFieldValidator } from '.';
 import { ValidationFunction } from './types';
@@ -9,11 +10,13 @@ import { hasValue } from './validationUtils';
 
 dayjs.extend(isSameOrAfter);
 dayjs.extend(isSameOrBefore);
+dayjs.extend(isoWeek);
 
 export enum ValidateDateError {
     invalidDateFormat = 'invalidDateFormat',
     dateBeforeMin = 'dateBeforeMin',
     dateAfterMax = 'dateAfterMax',
+    dateNotWeekday = 'dateNotWeekday',
 }
 
 export type DateValidationResult =
@@ -21,18 +24,20 @@ export type DateValidationResult =
     | ValidateDateError.invalidDateFormat
     | ValidateDateError.dateBeforeMin
     | ValidateDateError.dateAfterMax
+    | ValidateDateError.dateNotWeekday
     | undefined;
 
 export interface DateValidationOptions {
     required?: boolean;
     min?: Date;
     max?: Date;
+    onlyWeekdays?: boolean;
 }
 
 const getDateValidator = (options: DateValidationOptions = {}): ValidationFunction<DateValidationResult> => (
     value: any
 ) => {
-    const { required, min, max } = options;
+    const { required, min, max, onlyWeekdays } = options;
     const date = datepickerUtils.getDateFromDateString(value);
     if (required) {
         const err = getRequiredFieldValidator()(value);
@@ -50,6 +55,9 @@ const getDateValidator = (options: DateValidationOptions = {}): ValidationFuncti
         }
         if (max && dayjs(date).isAfter(max, 'day')) {
             return ValidateDateError.dateAfterMax;
+        }
+        if (onlyWeekdays && dayjs(date).isoWeekday() > 5) {
+            return ValidateDateError.dateNotWeekday;
         }
     }
     return undefined;
