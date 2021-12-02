a73e52d6e60cea7ef7206c2ba047cba15bd0b05e
Frode Hansen <frode.hansen@bekk.no>
2021-12-02 11:10:49 +0100

    Validere checked === false

--- src/typed-formik-form/validation/__tests__/validateChecked.test.ts
+++ src/typed-formik-form/validation/__tests__/validateChecked.test.ts
@@ -10,6 +10,9 @@ describe(`validateChecked`, () => {
     it(`returns ${ValidateCheckedError.notChecked} when value is not defined`, () => {
         expect(getCheckedValidator()(undefined)).toBe(ValidateCheckedError.notChecked);
     });
+    it(`returns ${ValidateCheckedError.notChecked} when value is false`, () => {
+        expect(getCheckedValidator()(false)).toBe(ValidateCheckedError.notChecked);
+    });
     it(`returns ${ValidateCheckedError.notChecked} when value is an empty array`, () => {
         expect(getCheckedValidator()([])).toBe(ValidateCheckedError.notChecked);
     });
