df0dc3bf044b4d345039411518085b13770b4889
Aleksei Poroshin <69150024+poroshinaleksei@users.noreply.github.com>
2023-03-16 15:23:27 +0100

    Fiks kosovo2 (#632)
    * La til countryIsMemberOfEøsOrEfta
    
    * 23.5.0

--- src/typed-formik-form/utils/countryUtils.ts
+++ src/typed-formik-form/utils/countryUtils.ts
@@ -3,6 +3,50 @@ import * as countries from 'i18n-iso-countries';
 countries.registerLocale(require('i18n-iso-countries/langs/nb.json'));
 countries.registerLocale(require('i18n-iso-countries/langs/nn.json'));
 
+export const filteredListEØSCountries = (countryOptionValue: string, shouldFilter?: boolean) => {
+    if (shouldFilter) {
+        switch (countryOptionValue) {
+            case 'BE':
+            case 'BG':
+            case 'DK':
+            case 'EE':
+            case 'FI':
+            case 'FR':
+            case 'GR':
+            case 'IE':
+            case 'IS':
+            case 'IT':
+            case 'HR':
+            case 'CY':
+            case 'LV':
+            case 'LI':
+            case 'LT':
+            case 'LU':
+            case 'MT':
+            case 'NL':
+            case 'PL':
+            case 'PT':
+            case 'RO':
+            case 'SK':
+            case 'SI':
+            case 'ES':
+            case 'GB':
+            case 'SE':
+            case 'CZ':
+            case 'DE':
+            case 'HU':
+            case 'AT':
+            case 'CH':
+                return true;
+            default:
+                return false;
+        }
+    } else {
+        // Filter ut Antarktis
+        return countryOptionValue !== 'AQ';
+    }
+};
+
 export const getLocaleKey = (locale: string): string => {
     switch (locale) {
         case 'nn':
@@ -28,6 +72,15 @@ export const getAlpha3Code = (alpha2Code: string) => {
     // i18n-iso-countries 7.5.0 bruker 'XKX' 'alpha3Code' for Kosovo. 'XXK' kode brukes i NAV.
     // Endrer i18n-iso-countries sin landkode til landkode som brukes i NAV for å sende riktig kode videre.
     return countryAlpha3Code === 'XKX' ? 'XXK' : countryAlpha3Code;
+};
+
+export const countryIsMemberOfEøsOrEfta = (isoCode: string) => {
+    let isoCodeToUse = isoCode.toUpperCase();
+    if (isoCodeToUse === 'XXK') {
+        isoCodeToUse = 'XKX';
+    }
+    isoCodeToUse = isoCodeToUse.length === 2 ? isoCodeToUse : countries.alpha3ToAlpha2(isoCodeToUse);
+    return filteredListEØSCountries(isoCodeToUse.toUpperCase(), true) === true;
 };
 
 export const getCountries = () => countries;
